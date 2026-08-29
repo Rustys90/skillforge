40f0d1496e33f1deb2ce9bb85f51e11a974a6a03
Rustys90 <s90.vfx@gmail.com>
2026-08-29 19:18:34 +0530

    brand: app icon Cool revival

--- app/icon.js
+++ app/icon.js
@@ -13,9 +13,9 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#0A0A0A",
+          background: "#050B12",
           borderRadius: 14,
-          border: "1px solid #2D2D2D",
+          border: "1px solid #0F2430",
         }}
       >
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -24,8 +24,8 @@ export default function Icon() {
               width: 10,
               height: 10,
               borderRadius: 999,
-              background: "#BF00FF",
-              boxShadow: "0 0 0 4px rgba(191,0,255,0.25)",
+              background: "#00FFFF",
+              boxShadow: "0 0 0 4px rgba(0,255,255,0.25)",
               marginBottom: 2,
             }}
           />
@@ -33,7 +33,7 @@ export default function Icon() {
             style={{
               width: 28,
               height: 18,
-              border: "2.5px solid #2CFF05",
+              border: "2.5px solid #00FF52",
               borderBottom: "none",
               borderTopLeftRadius: 14,
               borderTopRightRadius: 14,
@@ -43,14 +43,14 @@ export default function Icon() {
             style={{
               width: 36,
               height: 8,
-              background: "#2D2D2D",
+              background: "#0F2430",
               borderRadius: 2,
               display: "flex",
               alignItems: "flex-end",
               justifyContent: "center",
             }}
           >
-            <div style={{ width: 28, height: 2, background: "#2CFF05", borderRadius: 2 }} />
+            <div style={{ width: 28, height: 2, background: "#00FF52", borderRadius: 2 }} />
           </div>
         </div>
       </div>
