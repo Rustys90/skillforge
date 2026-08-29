92141d0881ee158de70897915dcca7f2e97aa8ae
Rustys90 <s90.vfx@gmail.com>
2026-08-29 19:43:11 +0530

    brand: app icon Alchemical reaction

--- app/icon.js
+++ app/icon.js
@@ -13,9 +13,9 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#050B12",
+          background: "#0B0710",
           borderRadius: 14,
-          border: "1px solid #0F2430",
+          border: "1px solid #1A1224",
         }}
       >
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -24,8 +24,8 @@ export default function Icon() {
               width: 10,
               height: 10,
               borderRadius: 999,
-              background: "#00FFFF",
-              boxShadow: "0 0 0 4px rgba(0,255,255,0.25)",
+              background: "#9D00FF",
+              boxShadow: "0 0 0 4px rgba(157,0,255,0.28)",
               marginBottom: 2,
             }}
           />
@@ -33,7 +33,7 @@ export default function Icon() {
             style={{
               width: 28,
               height: 18,
-              border: "2.5px solid #00FF52",
+              border: "2.5px solid #6BFF00",
               borderBottom: "none",
               borderTopLeftRadius: 14,
               borderTopRightRadius: 14,
@@ -43,14 +43,14 @@ export default function Icon() {
             style={{
               width: 36,
               height: 8,
-              background: "#0F2430",
+              background: "#1A1224",
               borderRadius: 2,
               display: "flex",
               alignItems: "flex-end",
               justifyContent: "center",
             }}
           >
-            <div style={{ width: 28, height: 2, background: "#00FF52", borderRadius: 2 }} />
+            <div style={{ width: 28, height: 2, background: "#6BFF00", borderRadius: 2 }} />
           </div>
         </div>
       </div>
